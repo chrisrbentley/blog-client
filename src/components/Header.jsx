a6d5860a74bcc9b51c38aa45b0aceffa56fae733
Chris Bentley <chrisrbentley@pm.me>
2023-12-19 23:47:30 -0500

    Implemented header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,11 @@
-const Header = () => {};
+import styles from './Header.module.css';
+
+const Header = () => {
+	return (
+		<header>
+			<h1 className={styles.h1}>Odin Blog</h1>
+		</header>
+	);
+};
 
 export default Header;
