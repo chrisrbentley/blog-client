e5ca1a75ecc4464e0e20af14f49e1186e3449877
Chris Bentley <chrisrbentley@pm.me>
2024-01-06 01:21:36 -0500

    Added load screen to home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import { getPosts } from './api/posts';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Preview from './components/Preview';
+import ReactLoading from 'react-loading';
 
 function App() {
-	const [posts, setPosts] = useState([]);
+	const [posts, setPosts] = useState(null);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -16,7 +17,7 @@ function App() {
 		fetchData();
 	}, []);
 
-	return (
+	return posts ? (
 		<>
 			<Header />
 			<main>
@@ -32,6 +33,15 @@ function App() {
 				</div>
 			</main>
 		</>
+	) : (
+		<div className="loadingContainer">
+			<ReactLoading
+				type={'spinningBubbles'}
+				color={'#000000'}
+				height={'5%'}
+				width={'5%'}
+			/>
+		</div>
 	);
 }
 
